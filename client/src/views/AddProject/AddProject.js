d14af389a24154dff88242bc5d0f61da60831160
Megan Coppins <meganlcoppins@gmail.com>
2020-04-14 19:59:58 -0700

    created add project view, form, and added functionality

--- client/src/views/AddProject/AddProject.js
+++ client/src/views/AddProject/AddProject.js
@@ -1,18 +1,69 @@
-import React from "react";
-import { useAuth0 } from "../../utils/auth0Provider";
+import React, { useState, useEffect } from "react";
+// import { useAuth0 } from "../../utils/auth0Provider";
 import "./style.css";
+import API from "../../utils/API";
+import { Link } from "react-router-dom";
+import { Input, TextArea, FormBtn } from "../../components/Form/index";
 
 function AddProject() {
-    const { isAuthenticated, user } = useAuth0();
-    return(
-            <div className="row">
-                <div className="col">
-                    <h1>Add A Project</h1>
-                    {isAuthenticated && <p>{JSON.stringify(user, null, 2)}</p>}
-                </div>
-            </div>
-    )
+  //   const { isAuthenticated, user } = useAuth0();
+
+//   components initial state
+  const [formObject, setFormObject] = useState({})
+
+//   update component state when user types in input field
+    function handleInputChange(e){
+        const { name, value } =
+        e.target;
+        setFormObject({... formObject, [name]: value})
+    };
+
+// when form is submitted use API.saveProject method to save book data
+    function handleFormSubmit(e){
+        e.preventDefault();
+        if(formObject.project && formObject.description) {
+            API.createProject({
+                project: formObject.project,
+                description: formObject.description
+            })
+            .catch(err => console.log(err));
+        }
+    }
+
+  return (
+    <div className="container mt-4">
+      <div className="row">
+        <div className="col">
+          <h1>Add a New Project</h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <form>
+            <Input
+              onChange={handleInputChange}
+              name="project"
+              placeholder="project (required)"
+            />
+            <TextArea
+              onChange={handleInputChange}
+              name="description"
+              placeholder="Description (required)"
+            />
+            <FormBtn><Link to={"/"}>Done</Link></FormBtn>
+            <FormBtn
+            disabled={!(formObject.project && formObject.description)}
+            onClick={handleFormSubmit}
+            >
+              Save Project
+            </FormBtn>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
+/* {isAuthenticated && <p>{JSON.stringify(user, null, 2)}</p>} */
 
 export default AddProject;
