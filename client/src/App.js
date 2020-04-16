c1bdb925272bbfeda05ac24081cbe3429baf4ff1
Megan Coppins <meganlcoppins@gmail.com>
2020-04-16 14:36:53 -0700

    added private routes and profile page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Router, Route, Switch } from "react-router-dom";
 import history from "./utils/history";
 
 import { useAuth0 } from "./utils/auth0Provider";
+import PrivateRoute from "../src/components/PrivateRoute";
 
 import "./App.css";
 
@@ -12,19 +13,26 @@ import Navbar from "./common/Navbar";
 // Views
 import Home from "./views/Home";
 import AddTask from "./views/AddTask/AddTask";
+import Profile from "../src/views/Profile/Profile";
 // import Project from "./views/Project";
 
 // import Popup from "./components/popup";
 // import logo from "./components/images/logo7.png";
 
 function App() {
+  const { loading } = useAuth0();
+  if (loading) {
+    return <div>Loading...</div>;
+  }
   return (
     <Router history={history}>
       <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/tasks/add" component={AddTask} />
+        <Route path="/profile" component={Profile}/>
         {/* <Route path="/project" component={Project} /> */}
+        <PrivateRoute path="/tasks/add" component={AddTask} />
       </Switch>
     </Router>
   );
