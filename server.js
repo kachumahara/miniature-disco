15307706b48e081d9af3217147be0bee0aeb8f21
Ian <ian.shane.sears@gmail.com>
2020-04-16 17:50:13 -0700

    castusp;

--- server.js
+++ server.js
@@ -10,33 +10,30 @@ const http = require('http').createServer()
 const io = require('socket.io')(http);
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+    app.use(express.static("client/build"));
 }
 // Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/strategyscope",
-  {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/strategyscope", {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+    useUnifiedTopology: true
+});
 
 io.on('connection', (socket) => {
-  socket.on('message', (message) => {
-      socket.broadcast.emit('message', message)
-  })
+    socket.on('message', (message) => {
+        socket.broadcast.emit('message', message)
+    })
 })
 
 http.listen(socketPort)
 console.log("running on port:" + socketPort)
 // Start the API server
 app.listen(PORT, function () {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
