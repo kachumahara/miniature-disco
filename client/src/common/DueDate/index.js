4fd6c80c5cd0b5169297075efbf53bc64c87e8ad
Ian <ian.shane.sears@gmail.com>
2020-04-17 08:20:58 -0700

    getting the json, just trying to spit it out to page

--- client/src/common/DueDate/index.js
+++ client/src/common/DueDate/index.js
@@ -1,36 +1,70 @@
-import React from "react";
 import "./style.css";
-const db = require("../../models/task");
+// const db = require("../../models/task");
+import React, { useState, useEffect } from "react";
+import API from "../../utils/API";
+import uuid from "uuid/v4";
+const { v4: uuidv4 } = require("uuid");
+uuidv4();
 
 
+    /// using UUID, how to integrate that to backend(Mongodb). These are the projects(items) we are going to drag around.
+  
+    //// ajax call to retrieve data from seed (Task)
+    function DueDate() {
+    const [tasks, setTasks] = useState([]);
+    // const [formObject, setFormObject] = useState({})
+    console.table(tasks)
+    useEffect(() => {
+      loadTasks();
+    }, []);
+  
+    function loadTasks() {
+      API.getTasks()
+        .then((res) => {
+          setTasks(res.data);
+        })
+        .catch((err) => console.log(err));
+    }
+    
 
+    return( 
+            <div className="sidenav">
+            <ul>
+            <li id='sideLi'>{tasks.title}</li>
 
-function DueDate() {
-
-    return (
-
-       <div className="sidenav">
- <ul>
- <li id='sideLi'><p>Shyaboi</p><p>10/24/3030</p></li>
- <li id='sideLi'><p>Shyaboi</p><p>10/24/3030</p></li>
- <li id='sideLi'><p>Shyaboi</p><p>10/24/3030</p></li>
- <li id='sideLi'><p>Shyaboi</p><p>10/24/3030</p></li>
- <li id='sideLi'><p>Shyaboi</p><p>10/24/3030</p></li>
- <li id='sideLi'><p>Shyaboi</p><p>10/24/3030</p></li>
- <li id='sideLi'><p>Shyaboi</p><p>10/24/3030</p></li>
- <li id='sideLi'><p>Shyaboi</p><p>10/24/3030</p></li>
-
-
- 
-</ul>
- 
- 
-</div>
-
- 
-
-
-    );
+                <li id='sideLi'>Shyaboi</li>
+            </ul>
+            </div>
+        )
 }
-
 export default DueDate;
+
+
+// function DueDate() {
+
+//     return (
+
+//        <div className="sidenav">
+//  <ul>
+//  <li id='sideLi'><p>Shyaboi</p><p>10/24/3030</p></li>
+//  <li id='sideLi'><p>Shyaboi</p><p>10/24/3030</p></li>
+//  <li id='sideLi'><p>Shyaboi</p><p>10/24/3030</p></li>
+//  <li id='sideLi'><p>Shyaboi</p><p>10/24/3030</p></li>
+//  <li id='sideLi'><p>Shyaboi</p><p>10/24/3030</p></li>
+//  <li id='sideLi'><p>Shyaboi</p><p>10/24/3030</p></li>
+//  <li id='sideLi'><p>Shyaboi</p><p>10/24/3030</p></li>
+//  <li id='sideLi'><p>Shyaboi</p><p>10/24/3030</p></li>
+
+
+ 
+// </ul>
+ 
+ 
+// </div>
+
+ 
+
+
+//     );
+// }
+
