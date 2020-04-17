db0ccd8d4564c4af7af7b2fccdbc3530ddfd304e
Ian <ian.shane.sears@gmail.com>
2020-04-16 21:36:05 -0700

    logo added to splash

--- client/src/views/SplashPage/index.js
+++ client/src/views/SplashPage/index.js
@@ -1,30 +1,30 @@
 import React, { Fragment } from "react";
 import { useAuth0 } from "../../utils/auth0Provider";
-import './style.css';
-import Home from "../../views/Home";
-import App from '../../App'
-
+import "./style.css";
+import logo from "../../components/images/logo6.png";
 
 const Splash = () => {
-
-  const {isAuthenticated, loading, loginWithPopup, logout, user} = useAuth0();
+  const { isAuthenticated, loading, loginWithPopup, logout, user } = useAuth0();
   if (loading || !user) {
-      return <div id='main'><button onClick={
-      () => loginWithPopup()
-    } class="glow-on-hover" type="button">Strategy Scope
-   </button>;
-  </div>
+    return (
+      <div id="main">
+        <button
+          onClick={() => loginWithPopup()}
+          class="glow-on-hover"
+          type="button"
+        >
+          <img id="logo" src={logo} alt="logo" />
+        </button>
+        ;
+      </div>
+    );
   }
 
   if (isAuthenticated == true) {
-      alert('nice')
-
+    alert("nice");
   }
 
-  return (
-
-    <div id='nothing'></div>
-    );
+  return <div id="nothing"></div>;
 };
 
 export default Splash;
