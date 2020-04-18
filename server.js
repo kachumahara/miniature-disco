6b8f806ffa7fee54a83a3f6a9006dc01ca632e5d
Justin <jlkellogg18@outlook.com>
2020-04-17 22:52:31 -0700

    updated server js to remove redundant code

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 const express = require("express");
-
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const socketPort =  process.env.PORT || 5000;
-const http = require('http').createServer()
-const io = require('socket.io')(http);
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
 // Define middleware here
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+  app.use(express.static("client/build"));
 }
 // Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/strategyscope", {
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/strategyscope",
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
@@ -31,15 +31,11 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/strategyscope",
 //   console.log("MongoDB database connection established successfully");
 // });
 
-io.on('connection', (socket) => {
-    socket.on('message', (message) => {
-        socket.broadcast.emit('message', message)
-    })
-})
-
-http.listen(socketPort)
-console.log("running on port:" + socketPort)
-// Start the API server
-app.listen(PORT, function () {
-    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+io.on("connection", (socket) => {
+  socket.on("message", (message) => {
+    socket.broadcast.emit("message", message);
+  });
 });
+
+http.listen(PORT);
+console.log("running on port:" + PORT);
